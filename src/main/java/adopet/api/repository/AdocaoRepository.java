c59dc4d71c6f20fee2b55c35ad763749848763d9
Vinicius Louzada <vinelouzada@gmail.com>
2023-11-13 08:54:31 -0300

    Projeto inicial

---
+++ src/main/java/adopet/api/repository/AdocaoRepository.java
@@ -0,0 +1,11 @@
+package adopet.api.repository;
+
+import adopet.api.model.Adocao;
+import adopet.api.model.StatusAdocao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdocaoRepository extends JpaRepository<Adocao,Long> {
+
+    Integer countByTutorId(Long idTutor);
+    Integer countByTutorIdAndStatus(Long idTutor, StatusAdocao status);
+}
